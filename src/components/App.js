08178455951165d9d697a28a42911cedac1b1d6b
Jesse Hang <hangtimee@gmail.com>
2019-06-18 18:02:27 -0700

    [api] linked up youtube api

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
-import React from "react";
-import SearchBar from "./SearchBar";
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import KEY from '../apis/key';
+import VideoList from './VideoList';
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+        q: term
+      }
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
